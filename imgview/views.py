efa2521eec1d2fbb0e554ec34f5e95910377ea36
Mudi Yang <mudi.yang@yale.edu>
2020-08-25 17:21:52 -0500

    second try to commit edits on views.py and image.html: added static image

--- imgview/views.py
+++ imgview/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+from ipware import get_client_ip   
+import os
+	
 def image(request):
-    return render(request,'imgview/image.html',{'title':'Image'})
+	IP = getIP(request)
+	file = "/home/people/ss99/gwas/gwaspage/static/data/%sstatic.bed"%IP
+	os.system('./static/static_image.sh %s /home/people/ss99/gwas/gwaspage/static/image/static.png'%IP)
+
+	return render(request,'imgview/image.html',{'title':'Image'})
+
+def getIP(request):
+		ip, _ = get_client_ip(request)
+		if ip is None:
+			# Unable to get the client’s IP address
+			return “0.0.0.0”
+		else:
+			return ip
+
+# def image(request):
+#     return render(request,'imgview/image.html',{'title':'Image'})
